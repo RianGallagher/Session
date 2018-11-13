cb91236fa36fe23009087d6bac084a9a84dce43d
OrrinBlake <orrinblake@gmail.com>
2018-11-13 21:06:43 +0000

    remove unused imports

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation';
-import GridView, { SuperGridSectionList } from 'react-native-super-grid';
-import Collapsible from 'react-native-collapsible';
 import AltLogin from './components/AltLogin';
 import SpotifyLoginInitial from './components/SpotifyLoginInitial';
 import UserAssertion from './components/UserAssertion';
